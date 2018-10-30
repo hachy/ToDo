5629e991dd6ea095ab0454d7ebe57f714bd50cad
hachy <1613863+hachy@users.noreply.github.com>
2018-10-30 23:54:50 +0900

    fix DialogFragment
    - use the Support Library DialogFragment because DialogFragment was deprecated in API level 28.
    - fix `Avoid passing null as the view root` warning
    https://stackoverflow.com/a/37761900

--- app/src/main/java/io/github/hachy/android/todo/HeaderDialogFragment.kt
+++ app/src/main/java/io/github/hachy/android/todo/HeaderDialogFragment.kt
@@ -2,9 +2,10 @@ package io.github.hachy.android.todo
 
 import android.app.AlertDialog
 import android.app.Dialog
-import android.app.DialogFragment
 import android.os.Bundle
+import android.support.v4.app.DialogFragment
 import android.text.TextUtils
+import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.EditText
 
@@ -13,25 +14,26 @@ class HeaderDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         super.onCreateDialog(savedInstanceState)
         val builder = AlertDialog.Builder(activity)
-        val inflater = activity.layoutInflater
-        val view = inflater.inflate(R.layout.dialog_add_header, null)
-        val editText = view.findViewById<EditText>(R.id.editHeader)
+        val inflater = activity?.layoutInflater
+        val nullParent: ViewGroup? = null
+        val view = inflater?.inflate(R.layout.dialog_add_header, nullParent)
+        val editText = view?.findViewById<EditText>(R.id.editHeader)
 
         builder.setMessage(R.string.header)
                 .setView(view)
-                .setPositiveButton(R.string.add, { _, _ ->
-                    val header = "${editText.text}"
+                .setPositiveButton(R.string.add) { _, _ ->
+                    val header = "${editText?.text}"
                     if (!TextUtils.isEmpty(header)) {
                         (activity as MainActivity).doPositiveClick(header)
-                        editText.text.clear()
+                        editText?.text?.clear()
                     }
-                })
-                .setNegativeButton(R.string.cancel, { _, _ ->
+                }
+                .setNegativeButton(R.string.cancel) { _, _ ->
                     dismiss()
-                })
+                }
 
         val d = builder.create()
-        d.window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
+        d.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
 
         return d
     }
