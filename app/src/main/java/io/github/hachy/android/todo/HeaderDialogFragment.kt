99dc3892ab84b73b04a8ea6c00826aa8ef0fa202
hachy <1613863+hachy@users.noreply.github.com>
2018-11-01 09:44:57 +0900

    add error message when EditText is empty
    - prevent a dialog from closing when a PositiveButton is clicked
    - use TextInputLayout to display error message
    - use addTextChangedListener to observe whether the value of EditText is not empty
    
    https://stackoverflow.com/questions/2620444/how-to-prevent-a-dialog-from-closing-when-a-button-is-clicked
    https://guides.codepath.com/android/Working-with-the-EditText

--- app/src/main/java/io/github/hachy/android/todo/HeaderDialogFragment.kt
+++ app/src/main/java/io/github/hachy/android/todo/HeaderDialogFragment.kt
@@ -3,8 +3,11 @@ package io.github.hachy.android.todo
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.support.design.widget.TextInputLayout
 import android.support.v4.app.DialogFragment
+import android.text.Editable
 import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.EditText
@@ -17,23 +20,37 @@ class HeaderDialogFragment : DialogFragment() {
         val inflater = activity?.layoutInflater
         val nullParent: ViewGroup? = null
         val view = inflater?.inflate(R.layout.dialog_add_header, nullParent)
+        val errMsg = view?.findViewById<TextInputLayout>(R.id.text_input_header)
         val editText = view?.findViewById<EditText>(R.id.editHeader)
 
-        builder.setMessage(R.string.header)
-                .setView(view)
-                .setPositiveButton(R.string.add) { _, _ ->
-                    val header = "${editText?.text}"
-                    if (!TextUtils.isEmpty(header)) {
-                        (activity as MainActivity).doPositiveClick(header)
-                        editText?.text?.clear()
-                    }
-                }
-                .setNegativeButton(R.string.cancel) { _, _ ->
-                    dismiss()
-                }
+        val d = builder.setView(view)
+                .setPositiveButton(R.string.add, null)
+                .setNegativeButton(R.string.cancel) { _, _ -> dismiss() }
+                .create()
 
-        val d = builder.create()
         d.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
+        d.show()
+        d.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
+            val header = "${editText?.text}"
+            if (TextUtils.isEmpty(header)) {
+                errMsg?.error = getString(R.string.header_empty)
+            } else {
+                (activity as MainActivity).doPositiveClick(header)
+                editText?.text?.clear()
+                dismiss()
+            }
+        }
+
+        editText?.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (!TextUtils.isEmpty("${editText.text}")) {
+                    errMsg?.error = null
+                    errMsg?.isErrorEnabled = false
+                }
+            }
+        })
 
         return d
     }
