29e4b4ae21e940045e31bff3c97f6c42068991c8
hachy <1613863+hachy@users.noreply.github.com>
2018-11-04 13:37:19 +0900

    add toolbar in settings

--- app/src/main/java/io/github/hachy/android/todo/SettingsActivity.kt
+++ app/src/main/java/io/github/hachy/android/todo/SettingsActivity.kt
@@ -4,13 +4,29 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         supportFragmentManager.beginTransaction().replace(R.id.container, SettingsFragment.newInstance()).commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     companion object {
